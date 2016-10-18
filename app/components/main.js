2966db632c1c191e52f80b10dce7291371be6308
andela-tkomolafe <tolulope.komolafe@andela.co>
2016-10-18 23:30:59 +0000

    add main component

---
+++ app/components/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayName = 'Feed-finder';
+  }
+  render() {
+    return <div>This is main</div>;
+  }
+}
+
