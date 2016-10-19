b99551188fd572295656da8f0e51aae019ecfbb2
andela-tkomolafe <tolulope.komolafe@andela.co>
2016-10-19 17:23:51 +0000

    add searchFeeds Component

---
+++ app/components/SearchFeeds.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default class SearchFeeds extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {url: ''}
+    this.fetchFeeds = this.fetchFeeds.bind(this)
+    this.onInputChange = this.onInputChange.bind(this)
+  }
+  onInputChange(e) {
+    this.setState({url: e.target.value})
+  }
+  fetchFeeds() {
+    this.props.fetchFeeds(this.state.url)
+    this.setState({url: ''})
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Enter the url to the feeds you want.</p>
+          <input type='text' onChange={this.onInputChange} value={this.state.url} placeholder='https://github.com/betty.atom'/>
+          <button onClick={this.fetchFeeds}>Search</button>
+      </div>
+    )
+  }
+}
