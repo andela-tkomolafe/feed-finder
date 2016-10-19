89b0641ea82ec98e4447842176138b0d850ed50a
andela-tkomolafe <tolulope.komolafe@andela.co>
2016-10-19 20:27:22 +0000

    display error if feeds is not available

--- app/actions/FeedAction.js
+++ app/actions/FeedAction.js
@@ -7,15 +7,18 @@ export const fetchFeeds = (url) => {
     url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
     dataType: 'json'
   }).done((xml) => {
-            let values = xml.responseData.feed
+            let values = xml.responseData
+            console.log(values, "values")
+
             dispatch({
                 type: 'GOT_FEEDS',
                 data: values
             });
         }).fail(() => {
+          console.log("here")
             dispatch({
                 type: 'GOT_FEEDS_FAIL',
-                data: {error: 'Unable to load feed, Incorrect path or invalid feed'}
+                data: 'Unable to load feed, Incorrect path or invalid feed'
             });
         });
   }
--- app/components/FeedsList.js
+++ app/components/FeedsList.js
@@ -4,29 +4,40 @@ import '../../stylesheets/modules/feedslist.css'
 
 export default class FeedsList extends React.Component {
   render() {
-    let feeds = this.props.feeds
-      return (
-        <div className='feeds'>
-          {feeds.title ? <h2>{feeds.title}</h2> : null}
-          {feeds.description ? <p>{feeds.description}</p> : null}
-          <div >
-            {feeds.entries.map((feed, index) => {
-              return(
-                <div key={index} className='feed-box'>
-                  <p>{feed.title}</p>
-                  <p className='bold'>Author: {feed.author}</p>
-                  <a href={feed.link}>{feed.link}</a>
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      )
+    return (
+      <div className='feeds'>
+        {this.props.feeds ? <List feeds={this.props.feeds.feed} /> : <p>Unable to load feed, Incorrect path or invalid feed.</p>}
+      </div>
+    )
   }
 }
 
 FeedsList.defaultProps = {
   feeds: {
+    title: '',
+    description: '',
     entries: []
   }
+}
+
+
+const List = props => {
+  let feeds = props.feeds
+  return(
+    <div>
+      {feeds.title ? <h2>{feeds.title}</h2> : null}
+      {feeds.description ? <p>{feeds.description}</p> : null}
+      <div >
+        {feeds.entries.map((feed, index) => {
+          return(
+            <div key={index} className='feed-box'>
+              {feed.title ? <p>{feed.title}</p> : null}
+              {feed.author ? <p className='bold'>Author: {feed.author}</p> : null}
+              {feed.link ? <a href={feed.link}>{feed.link}</a> : null }
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
