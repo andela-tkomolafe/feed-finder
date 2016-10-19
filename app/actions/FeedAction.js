f4ac82041be74e8bb74f6fe9371e454d7a70f524
andela-tkomolafe <tolulope.komolafe@andela.co>
2016-10-19 17:21:50 +0000

    add ajax call to action

--- app/actions/FeedAction.js
+++ app/actions/FeedAction.js
@@ -1,9 +1,23 @@
-import axios from 'axios'
-export const FeedsAction = (url) => {
-  let data = axios.get(url)
-  console.log(data)
-  return {
-    type: 'GOT_FEEDS',
-    data: data
+import $ from 'jquery'
+
+export const fetchFeeds = (url) => {
+  return (dispatch) => {
+  $.ajax({
+    type: "GET",
+    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
+    dataType: 'json'
+  }).done((xml) => {
+            let values = xml.responseData.feed
+            dispatch({
+                type: 'GOT_FEEDS',
+                data: values
+            });
+        }).fail(() => {
+            dispatch({
+                type: 'GOT_FEEDS_FAIL',
+                data: {error: 'Unable to load feed, Incorrect path or invalid feed'}
+            });
+        });
   }
 }
+
