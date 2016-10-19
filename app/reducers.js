91480951266aa714465eb8234d7a1c9b08e037ff
andela-tkomolafe <tolulope.komolafe@andela.co>
2016-10-19 20:31:22 +0000

    add failure action to reducer

--- app/reducers.js
+++ app/reducers.js
@@ -1,7 +1,9 @@
 export const feedFinderApp = (state = {}, action) => {
   switch (action.type) {
     case 'GOT_FEEDS':
-      return Object.assign({}, state, {data: action.data});
+      return Object.assign({}, state, {data: action.data})
+    case 'GOT_FEEDS_FAIL':
+      return Object.assign({}, state, {error: action.data})
     default:
       return state;
   }
