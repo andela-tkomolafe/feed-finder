ad0c891d80dd9b336698bf59f6e6a3451f014ebd
andela-tkomolafe <tolulope.komolafe@andela.co>
2016-10-19 20:35:57 +0000

    cleanup

--- app/actions/FeedAction.js
+++ app/actions/FeedAction.js
@@ -4,23 +4,20 @@ export const fetchFeeds = (url) => {
   return (dispatch) => {
   $.ajax({
     type: "GET",
-    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
+    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url), // pass the feed through google api to return a json
     dataType: 'json'
   }).done((xml) => {
-            let values = xml.responseData
-            console.log(values, "values")
-
-            dispatch({
-                type: 'GOT_FEEDS',
-                data: values
-            });
-        }).fail(() => {
-          console.log("here")
-            dispatch({
-                type: 'GOT_FEEDS_FAIL',
-                data: 'Unable to load feed, Incorrect path or invalid feed'
-            });
+          let values = xml.responseData
+          dispatch({
+              type: 'GOT_FEEDS',
+              data: values
+          });
+    }).fail(() => {
+        dispatch({
+            type: 'GOT_FEEDS_FAIL',
+            data: 'Unable to load feed, Incorrect path or invalid feed'
         });
+      });
   }
 }
 
